6151b973e9ff669843d23eb9cf5bce69d585893a
lucasneckel <lucas.neckel@gmail.com>
2020-03-25 11:58:46 -0300

    1a etapa curso

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//import React, { useState } from 'react';
+//import Header from './Header';
+
+import './global.css';
+
+import Logon from './pages/Logon';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Logon />
+  ) ;
 }
+
+
+
+/*const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+      <div>
+        <Header>Contador: {counter}</Header>
+        <button onClick={increment}>Incrementar</button>
+      </div>
+      
+    );
+}*/
+
 
 export default App;
